5d4c1bd4c3ba10d673e825556b77cb8f6aded892
Marco <marco.satta247@gmail.com>
2022-06-17 15:24:49 +0200

    aggiunta del css previo login

--- src/main/java/com/example/auth/securingweb/WebSecurityConfig.java
+++ src/main/java/com/example/auth/securingweb/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.auth.securingweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,20 +9,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin()
-		.loginPage("/login.html")
-		.defaultSuccessUrl("/home.html", true)
-		.failureUrl("/login.html?error=true");
+
+		String[] staticResources = {
+            "/css/**",
+            "/images/**",
+            "/templates/**"};
+
+			http.authorizeRequests()
+			.antMatchers("/register").permitAll()
+			.antMatchers("/login").permitAll().antMatchers(staticResources).permitAll()
+			.anyRequest()
+			.authenticated().and().formLogin().loginPage("/login").failureUrl("/login?error=true")
+			.usernameParameter("username")
+			.passwordParameter("password").and().logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").and()
+			.exceptionHandling().accessDeniedPage("/access-denied");
 	}
 
 	@Bean
